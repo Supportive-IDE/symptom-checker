2cbf20b12154d5c0a7969a1ddb25460fc9f9c45f
Abi Evans <50892754+ace01603@users.noreply.github.com>
2025-08-04 08:46:25 +0100

    updated vite config with correct base

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://ace01603.github.io/symptom-checker/",
+  base: "https://supportive-ide.github.io/symptom-checker/",
     build: {
         outDir: 'docs',
         emptyOutDir: true, // also necessary
